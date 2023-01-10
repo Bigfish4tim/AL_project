1f9099ff6ca7f8f8cbac136d7b89daf45219c41c
Bigfish4tim <bigfish.4tim@gmail.com>
2023-01-10 17:49:20 +0900

    01-10

--- src/AL_17406.java
+++ src/AL_17406.java
@@ -8,6 +8,46 @@ public class AL_17406 {
     static int N, M, K;
     static int[][] RMap;
 
+    public static int[][] rotation(int r, int c, int s) {
+        int[][] tempMap = map.clone();
+
+        for(int i=1; i<=s; i++) {
+            int size = i*2;
+            for(int j=0; j<size; j++) {
+                tempMap[r-i][c-i+j+1] = map[r-i][c-i+j];
+                tempMap[r-i+j+1][c+i] = map[r-i+j][c+i];
+                tempMap[r+i][c+i-j-1] = map[r+i][c+i-j];
+                tempMap[r+i-j-1][c-i] = map[r+i-j][c-i];
+            }
+        }
+
+//        int min = Integer.MAX_VALUE;
+//
+//        for(int i=0; i<N; i++) {
+//            for(int j=0; j<M; j++) {
+//                if(min > tempMap[i][j]) {
+//                    min = tempMap[i][j];
+//                }
+//            }
+//        }
+
+        return tempMap;
+    }
+
+    public static int findMin(int[][] staticMap) {
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(min > staticMap[i][j]) {
+                    min = staticMap[i][j];
+                }
+            }
+        }
+
+        return min;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,6 +71,13 @@ public class AL_17406 {
             for(int j=0; j<3; j++) {
                 RMap[i][j] = Integer.parseInt(st.nextToken());
             }
+        }
+
+        int[][] rotMap = new int[N][M];
+
+        for(int i=0; i<RMap.length; i++) {
+            rotMap = rotation(RMap[i][0], RMap[i][1], RMap[i][2]);
+
         }
     }
 }
