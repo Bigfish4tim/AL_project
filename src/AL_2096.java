9b42b199d93978bedf4740862f6f7c21c018cffd
Bigfish4tim <bigfish.4tim@gmail.com>
2023-01-30 16:30:55 +0900

    01-30

--- src/AL_2096.java
+++ src/AL_2096.java
@@ -6,6 +6,23 @@ import java.util.*;
 public class AL_2096 {
     static int N;
     static int[][] map;
+    static int max = 0;
+    static int min = Integer.MAX_VALUE;
+    static int[] pos;
+
+    public static void SlindingWindow() {
+        int sum = 0;
+
+        for(int i=0; i<N; i++) {
+            sum += map[i][0];
+        }
+
+        for(int i=N-1; i>=0; i--) {
+            for(int j=0; j<3; j++) {
+
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,9 +31,11 @@ public class AL_2096 {
         N = Integer.parseInt(st.nextToken());
 
         map = new int[N][3];
+        pos = new int[N];
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
+            pos[i] = 0;
             for(int j=0; j<3; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
