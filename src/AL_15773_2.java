0caac696840b49655cedefd37371bd7e024e5360
Bigfish4tim <bigfish.4tim@gmail.com>
2023-03-21 16:45:37 +0900

    03-21

--- src/AL_15773_2.java
+++ src/AL_15773_2.java
@@ -24,27 +24,23 @@ public class AL_15773_2 {
         public int compareTo(data o) {
             if(this.l > o.getL()) return 1;
             else if(this.l < o.getL()) return -1;
-            else {
-                return Integer.compare(this.d, o.getD());
-            }
+            else return Integer.compare(this.d, o.getD());
         }
     }
 
-    public static void starter(int start, int sum) {
+    public static void starter(int start, int sum, int cnt) {
+//        if(count < cnt) count = cnt;
+
         for(int i=start; i<arr.size(); i++) {
-            int temp = arr.get(i).getD();
+            if(arr.size() - i < count - cnt) break;
+            data temp = arr.get(i);
+            if(sum <= temp.l) starter(i+1, sum+temp.d, cnt+1);
         }
-    }
-
-    public static void dfs(int start, int sum) {
-
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        ArrayList<data> subArr = new ArrayList<>();
 
         n = Integer.parseInt(st.nextToken());
 
@@ -52,73 +48,12 @@ public class AL_15773_2 {
             st = new StringTokenizer(br.readLine());
             long tempL = Long.parseLong(st.nextToken());
             int tempD = Integer.parseInt(st.nextToken());
-
             arr.add(new data(tempL, tempD));
         }
         Collections.sort(arr);
 
-        int pos = 0;
+        starter(0, 0, 0);
 
-        int size = arr.size();
-        for(int i=0; i<size; i++) {
-            data temp = arr.get(i);
-            if(temp.l >= pos) {
-                pos += temp.d;
-                subArr.add(arr.remove(i));
-                size--;
-                i--;
-            }
-        }
-
-        count = subArr.size();
-
-        boolean trig = true;
-
-        while (!arr.isEmpty()) {
-            size = arr.size();
-            for (int i=0; i<size; i++) {
-                data temp = arr.get(i);
-
-                if(trig) {
-                    pos = 0;
-                    int j=0;
-                    while (subArr.get(j).l <= temp.l) {
-                        pos += subArr.get(j).d;
-                        j++;
-                        if (pos > temp.l) {
-                            j = j-1;
-                            if (temp.l == subArr.get(j).l && temp.d > subArr.get(j).d) {
-                                pos -= subArr.get(j).d;
-                                arr.remove(i);
-                                temp = arr.get(i);
-                            } else {
-                                pos -= subArr.remove(j).d;
-                            }
-                            if (subArr.size() == j) break;
-                        }
-                    }
-
-                    while (subArr.size() != j) {
-                        data temp2 = subArr.remove(j);
-                        arr.add(temp2);
-                    }
-                    trig = false;
-                    Collections.sort(arr);
-                }
-
-                size = arr.size();
-                if (temp.l >= pos) {
-                    pos += temp.d;
-                    subArr.add(arr.remove(i));
-                    size--;
-                    i--;
-                }
-            }
-            trig = true;
-            if (count < subArr.size()) {
-                count = subArr.size();
-            }
-        }
         System.out.println(count);
     }
 }
