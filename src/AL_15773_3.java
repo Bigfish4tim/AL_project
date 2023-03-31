7ea7afeef8d377283305a22ec6ef914da5b28754
Bigfish4tim <bigfish.4tim@gmail.com>
2023-03-31 11:33:22 +0900

    03-31

--- src/AL_15773_3.java
+++ src/AL_15773_3.java
@@ -22,28 +22,62 @@ public class AL_15773_3 {
             this.d = d;
         }
 
+//        @Override
+//        public int compareTo(data o) {
+//            if(this.l+this.d > o.getL()+o.getD()) return 1;
+//            else if(this.l+this.d < o.getL()+o.getD()) return -1;
+//            else return Long.compare(this.l, o.getL());
+//        }
+//        @Override
+//        public int compareTo(data o) {
+//            if(this.d > getD()) return 1;
+//            else if(this.d < o.getD()) return -1;
+//            else return Long.compare(this.l, o.getL());
+//        }
         @Override
         public int compareTo(data o) {
-            if(this.l+this.d > o.getL()+o.getD()) return 1;
-            else if(this.l+this.d < o.getL()+o.getD()) return -1;
-            else return Long.compare(this.l, o.getL());
+            if(this.l > getL()) return 1;
+            else if(this.l < o.getL()) return -1;
+            else return Long.compare(this.d, o.getD());
         }
     }
 
-    public static void step() {
+    public static int step() {
+        int listSum = 0;
         int sum = 0;
+        int cnt;
+        int check;
         while (!list.isEmpty()) {
+            listSum++;
+            cnt = 0;
+            check = -1;
             for(int i=0; i<list.size(); i++) {
+                list.sort(null);
                 data temp = list.remove(i);
-                data temp2;
+//                list.sort(new Comparator<data>() {
+//                    @Override
+//                    public int compare(data o1, data o2) {
+//                        if (o1.getL() > o2.getL()) return 1;
+//                        else if (o1.getL() < o2.getL()) return -1;
+//                        else return Integer.compare(o1.getD(), o2.getD());
+//                    }
+//                });
                 int tempSum = sum + temp.d;
-                int cnt = 0;
-                for(int j=0; j< list.size(); i++) {
-                    if(list.get(j).l > tempSum) cnt++;
+                int tempCheck = 0;
+                for(int j=0; j<list.size(); j++) {
+                    if (tempSum <= list.get(j).l) tempCheck++;
                 }
-
+                if (cnt < tempCheck) {
+                    cnt = tempCheck;
+                    check = i;
+                }
+                list.add(temp);
             }
+            list.sort(null);
+            if (check == -1) break;
+            sum += list.remove(check).d;
         }
+        return listSum;
     }
 
     public static void starter(int start, int sum, int cnt) {
@@ -60,6 +94,30 @@ public class AL_15773_3 {
         }
     }
 
+    public static void binarySearch(List<Integer> numbers, Integer value){
+        if(numbers == null || numbers.isEmpty()){
+            return;
+        }
+
+        final Integer comparison = numbers.get(numbers.size()/2);
+        System.out.println("기준값: "+comparison);
+        if(value.equals(comparison)){
+            System.out.print("결과: " + comparison);
+            return;
+        }
+
+        if(value<comparison){
+            //subList()메소드는 리스트의 일부를 추출함
+            binarySearch(numbers.subList(0, numbers.size()/2), value);
+        }else{
+            binarySearch(numbers.subList(numbers.size()/2+1, numbers.size()), value);
+        }
+    }
+
+    public static void starter2() {
+
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -74,18 +132,28 @@ public class AL_15773_3 {
             st = new StringTokenizer(br.readLine());
             long tempL = Long.parseLong(st.nextToken());
             int tempD = Integer.parseInt(st.nextToken());
-//            dataArr[i] = new data(tempL, tempD);
-            list.add(new data(tempL, tempD));
+            dataArr[i] = new data(tempL, tempD);
+//            list.add(new data(tempL, tempD));
         }
 
-//        Arrays.sort(dataArr);
+
+        Arrays.sort(dataArr);
 //
 //        starter(0,0,0);
 
-        list.sort(null);
+//        list.sort(null);
 
+//        list.sort(new Comparator<data>() {
+//            @Override
+//            public int compare(data o1, data o2) {
+//                if (o1.getL() > o2.getL()) return 1;
+//                else if (o1.getL() < o2.getL()) return -1;
+//                else return Integer.compare(o1.getD(), o2.getD());
+//            }
+//        });
 
-        System.out.println(count);
+//        System.out.println(count);
 
+        System.out.println(step());
     }
 }
