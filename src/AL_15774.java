64935540d134470fc7c5c57988fa522461e3fb58
Bigfish4tim <bigfish.4tim@gmail.com>
2023-04-13 17:39:26 +0900

    04-13

--- src/AL_15774.java
+++ src/AL_15774.java
@@ -8,13 +8,13 @@ public class AL_15774 {
     static house[] map;
 
     public static class house implements Comparable<house> {
-        int x;
-        int y;
+        long x;
+        long y;
 
-        public int getX() { return x; }
-        public int getY() { return y; }
+        public long getX() { return x; }
+        public long getY() { return y; }
 
-        public house(int x, int y) {
+        public house(long x, long y) {
             this.x = x;
             this.y = y;
         }
@@ -23,16 +23,16 @@ public class AL_15774 {
         public int compareTo(house o) {
             if (this.x > o.getX()) return 1;
             else if (this.x < o.getX()) return -1;
-            else return Integer.compare(this.y, o.getY());
+            else return Long.compare(this.y, o.getY());
         }
     }
 
-    public static int ccw(house A, house B, house C) {
-        long cal = 0;
-        cal = (long)(B.x - A.x) * (C.y - A.y) - (long)(C.x-A.x) * (B.y-A.y);
-        if(cal > 0)    return 1;
-        else if (cal< 0)    return -1;
-        else    return 0;
+    public static class houseList extends ArrayList<house> {
+
+    }
+
+    static long ccw(house p1, house p2, house p3) {
+        return (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.y * p2.x + p2.y * p3.x + p3.y * p1.x);
     }
 
     public static long dist(house p1, house p2) {
@@ -41,8 +41,80 @@ public class AL_15774 {
         return (dx*dx + dy*dy);
     }
 
-    public static house[] caliph() {
+    public static Stack<house> convexHull(ArrayList<house> input) {
+        house root = new house(0,0);
 
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i).x < root.x) {
+                root = input.get(i);
+            } else if (input.get(i).x == root.x) {
+                if (input.get(i).y < root.y) {
+                    root = input.get(i);
+                }
+            }
+        }
+
+        house finalRoot = root;
+        input.sort(new Comparator<house>() {
+            @Override
+            public int compare(house o1, house o2) {
+                long ccw = ccw(finalRoot, o1, o2);
+
+                if (ccw > 0) {
+                    return -1;
+                } else if (ccw < 0) {
+                    return 1;
+                } else {
+                    double distance1 = dist(finalRoot, o1);
+                    double distance2 = dist(finalRoot, o2);
+
+                    return Double.compare(distance1, distance2);
+                }
+            }
+        });
+
+        Stack<house> stack = new Stack<>();
+        stack.add(root);
+
+        for (int i=1; i<input.size(); i++) {
+            while (stack.size() > 1 && (ccw(stack.get(stack.size()-2), stack.get(stack.size()-1, input.get(i))) <= 0)) {
+                stack.pop();
+            }
+            stack.add(input.get(i));
+        }
+
+        return stack;
+    }
+
+    static long rotatingCalipers(ArrayList<house> convexHull) {
+        long max_dist = 0;
+
+        int j = 1;
+        for (int i = 0; i < convexHull.size(); i++) {
+            int i_next = (i + 1) % convexHull.size();
+            for (; ; ) {
+                int j_next = (j + 1) % convexHull.size();
+
+                long bx = convexHull.get(i_next).x - convexHull.get(i).x; // 왼쪽 벡터
+                long by = convexHull.get(i_next).y - convexHull.get(i).y;
+                long cx = convexHull.get(j_next).x - convexHull.get(j).x;   // 오른쪽 벡터
+                long cy = convexHull.get(j_next).y - convexHull.get(j).y;
+
+                long ccw = ccw(new house(0, 0), new house(bx, by), new house(cx, cy));
+                if (ccw > 0) {  // 반시계 방향이면 오른쪽에 있는 점을 다음으로
+                    j = j_next;
+                } else {    // 시계 방향이면 왼족에 있는 점을 다음으로
+                    break;
+                }
+            }
+
+            // 최대 거리 구하기
+            if (dist(convexHull.get(i), convexHull.get(j)) > max_dist) {
+                max_dist = dist(convexHull.get(i), convexHull.get(j));
+            }
+        }
+
+        return max_dist;
     }
 
     public static void main(String[] args) throws IOException {
@@ -61,5 +133,7 @@ public class AL_15774 {
         }
 
         Arrays.sort(map);
+
+
     }
 }
