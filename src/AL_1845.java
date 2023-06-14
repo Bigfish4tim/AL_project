fca5e1026a08722f31f8272c8e681c96bcfec653
Bigfish4tim <bigfish.4tim@gmail.com>
2023-06-14 17:59:41 +0900

    06-14

--- src/AL_1845.java
+++ src/AL_1845.java
@@ -1,65 +1,71 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AL_1845 {
+    static Map<List<Integer>, Integer> toSort;
+
+    public static void precalc(int n) {
+        List<Integer> perm = new ArrayList<>();
+        Queue<List<Integer>> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++)
+            perm.add(i);
+
+        q.offer(perm);
+        toSort.put(perm, 0);
+
+        while (!q.isEmpty()) {
+            List<Integer> here = q.poll();
+            int cost = toSort.get(here);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 2; j <= n; j++) {
+                    Collections.reverse(here.subList(i, j));
+                    if (!toSort.containsKey(here)) {
+                        toSort.put(new ArrayList<>(here), cost + 1);
+                        q.offer(new ArrayList<>(here));
+                    }
+                    Collections.reverse(here.subList(i, j));
+                }
+            }
+        }
+    }
+
+    public static int solve(List<Integer> perm) {
+        int n = perm.size();
+        List<Integer> fixed = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int smaller = 0;
+            for (int j = 0; j < n; j++) {
+                if (perm.get(i) > perm.get(j))
+                    smaller++;
+            }
+            fixed.add(smaller);
+        }
+
+        return toSort.get(fixed);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
-        }
+        int C = scanner.nextInt();
+        toSort = new HashMap<>();
+        precalc(8);
 
-        int[] finalArray = new int[N];
-        for (int i = 0; i < N; i++) {
-            finalArray[i] = scanner.nextInt();
-        }
+        while (C-- > 0) {
+            int N = scanner.nextInt();
+            List<Integer> v = new ArrayList<>();
+            int k = 1000001;
 
-        List<String> operations = new ArrayList<>();
-        boolean[] visited = new boolean[N];
-
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                List<Integer> cycle = new ArrayList<>();
-                int current = i;
-                while (!visited[current]) {
-                    cycle.add(current);
-                    visited[current] = true;
-                    current = findIndex(finalArray, arr[current]);
-                }
-
-                if (cycle.size() > 1) {
-                    reverse(arr, cycle);
-                    operations.add((cycle.get(0) + 1) + " " + (cycle.get(cycle.size() - 1) + 1));
-                }
+            for (int i = 0; i < N; i++) {
+                int tmp = scanner.nextInt();
+                v.add(tmp);
             }
-        }
 
-        System.out.println(operations.size());
-        for (String operation : operations) {
-            System.out.println(operation);
-        }
-    }
+            while (v.size() < 8)
+                v.add(k++);
 
-    private static int findIndex(int[] array, int value) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private static void reverse(int[] array, List<Integer> indices) {
-        int start = 0;
-        int end = indices.size() - 1;
-        while (start < end) {
-            int temp = array[indices.get(start)];
-            array[indices.get(start)] = -array[indices.get(end)];
-            array[indices.get(end)] = -temp;
-            start++;
-            end--;
+            System.out.println(solve(v));
         }
     }
 }
