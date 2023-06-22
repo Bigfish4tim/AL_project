5f35362f8f39d69ddff7cc7426c63bee9a1ad94d
Bigfish4tim <bigfish.4tim@gmail.com>
2023-06-22 17:00:52 +0900

    06-22

--- src/AL_1845_3.java
+++ src/AL_1845_3.java
@@ -7,6 +7,8 @@ public class AL_1845_3 {
     static point head;
     static point tail;
 
+    static Stack<range> q = new Stack<>();
+
     public static class point {
         int num;
         int index;
@@ -14,6 +16,16 @@ public class AL_1845_3 {
         public point(int num, int index) {
             this.num = num;
             this.index = index;
+        }
+    }
+
+    public static class range {
+        int to;
+        int from;
+
+        public range(int to, int from) {
+            this.to = to;
+            this.from = from;
         }
     }
 
@@ -27,10 +39,8 @@ public class AL_1845_3 {
             map[a+i] = -map[b-i];
             map[b-i] = -temp;
         }
-    }
 
-    public static boolean isc(int[] a, int[] b) {
-        return Arrays.equals(a, b);
+        q.add(new range(a,b));
     }
 
     public static void main(String[] args) {
@@ -44,19 +54,69 @@ public class AL_1845_3 {
             map[i+1] = scanner.nextInt();
         map[0] = 0;
         map[N+1] = N+1;
+        head = new point(0,0);
+        tail = new point(100000,0);
 
-        while (head.num != N+1) {
+        while (tail.num != N+1) {
             head = new point(0,0);
-            tail = new point(0,0);
+            tail = new point(100000,0);
             for (int i=1; i<N+1; i++) {
+                if (head.num > 0) {
+                    if (head.num+1 == map[i]) continue;
+                    else if (head.num < map[i]) {
+                        head = new point(map[i], i);
+                        tail = new point(map[i+1], i+1);
+                        continue;
+                    } else {
+
+                    }
+                } else {
+
+                }
                 if (Math.abs(head.num) < Math.abs(map[i])) {
                     head = new point(map[i], i);
+                    tail = new point(map[i+1], i+1);
                     continue;
                 }
-                if (Math.abs(tail.num - map[i]) != 1) {
-
+                if (tail.num == map[i]) continue;
+                if (map[i] >= 0) {
+                    while (Math.abs(tail.num - map[i]) == 1) {
+                        tail = new point(map[i], i);
+                        i++;
+                    }
+                    reverse(head.index, tail.index);
+                    break;
+                } else {
+                    while (tail.num - map[i] == -1) {
+                        tail = new point(map[i], i);
+                        i++;
+                    }
+                    reverse(head.index, tail.index);
+                    break;
                 }
             }
         }
+
+        int size = q.size();
+        System.out.println(size);
+        for (int i=0; i<size; i++) {
+            range temp = q.pop();
+
+            assert temp != null;
+            System.out.println(temp.to + " " + temp.from);
+        }
     }
 }
+
+
+
+/*
+6
+-1 -2 -3 -4 -5 -6
+
+6
+-6 -5 -4 -3 -2 -1
+
+8
+-4 -3 -5 1 2 7 6 8
+ */
