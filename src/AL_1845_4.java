60c2219916e4617910a21bace4a7c20d6df048af
Bigfish4tim <bigfish.4tim@gmail.com>
2023-06-28 17:59:45 +0900

    06-28

---
+++ src/AL_1845_4.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class AL_1845_4 {
+    static int N;
+    static int[] map;
+
+    static point head;
+    static point tail;
+
+    static Stack<range> q = new Stack<>();
+
+    public static class point {
+        int head;
+        int tail;
+        int index;
+
+        public point(int head, int tail, int index) {
+            this.head = head;
+            this.tail = tail;
+            this.index = index;
+        }
+    }
+
+    public static class range {
+        int to;
+        int from;
+
+        public range(int to, int from) {
+            this.to = to;
+            this.from = from;
+        }
+    }
+
+    public static void reverse(int a, int b) {
+        int range = (b-a+1);
+        int size = (int) Math.ceil((double) range / 2);
+
+        for(int i=0; i<size; i++) {
+            int temp;
+            temp = map[a+i];
+            map[a+i] = -map[b-i];
+            map[b-i] = -temp;
+        }
+
+        q.add(new range(a,b));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+        map = new int[N+2];
+
+        for (int i=0; i<N; i++)
+            map[i+1] = scanner.nextInt();
+        map[0] = 0;
+        map[N+1] = N+1;
+
+
+    }
+}
