dae1994c64b718cead4eaca56d1f260d48b9697d
Bigfish4tim <bigfish.4tim@gmail.com>
2023-07-25 17:53:40 +0900

    07-25

---
+++ src/AL_2834.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class AL_2834 {
+    static int N;
+    static int[] map;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+        map = new int[N];
+
+        for (int i=0; i<N; i++)
+            map[i] = scanner.nextInt();
+
+
+    }
+}
