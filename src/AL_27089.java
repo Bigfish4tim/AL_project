18e2de92a85d09819f1a8ceebe9e9d861d885730
Bigfish4tim <bigfish.4tim@gmail.com>
2023-08-08 17:59:26 +0900

    08-08

---
+++ src/AL_27089.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class AL_27089 {
+
+    static int N, K;
+
+    public static class Operator {
+        int x;
+        int y;
+        int sight;
+
+        public Operator(int x, int y, int sight) {
+            this.x = x;
+            this.y = y;
+            this.sight = sight;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        K = scanner.nextInt();
+
+        Operator[] map = new Operator[N];
+
+        for (int i=0; i<N; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int sight = scanner.nextInt();
+
+            map[i] = new Operator(x, y, sight);
+        }
+
+
+    }
+}
