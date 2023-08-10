180a66139a1c79614c925c35b4814925c5bbda40
Bigfish4tim <bigfish.4tim@gmail.com>
2023-08-10 17:52:35 +0900

    08-10

--- src/AL_27089.java
+++ src/AL_27089.java
@@ -7,13 +7,40 @@ public class AL_27089 {
     public static class Operator {
         int x;
         int y;
-        int sight;
+        double upSight;
+        double downSight;
 
-        public Operator(int x, int y, int sight) {
+        public Operator(int x, int y, double upSight, double downSight) {
             this.x = x;
             this.y = y;
-            this.sight = sight;
+            this.upSight = upSight;
+            this.downSight = downSight;
         }
+    }
+
+    public static void angleCheck(int sight) {
+        /*
+        +x = 0
+        -x = 1
+        +y = 2
+        -y = 3
+         */
+
+        if (sight == 0) {
+
+        }
+    }
+
+    public static double degrees(Operator a1, Operator a2) {
+        // 각도를 계산합니다.
+        double angleRadians = Math.atan2(a2.y -a1.y, a2.x - a1.x);
+        double angleDegrees = Math.toDegrees(angleRadians - Math.PI/4);
+
+        if (angleDegrees < 0) {
+            angleDegrees = 360 + angleDegrees;
+        }
+
+        return angleDegrees;
     }
 
     public static void main(String[] args) {
@@ -29,7 +56,14 @@ public class AL_27089 {
             int y = scanner.nextInt();
             int sight = scanner.nextInt();
 
-            map[i] = new Operator(x, y, sight);
+            if (sight == 0)
+                map[i] = new Operator(x, y, 90.0, 0.0);
+            else if (sight == 1)
+                map[i] = new Operator(x, y, 180.0, 90.0);
+            else if (sight == 2)
+                map[i] = new Operator(x, y, 270.0, 180.0);
+            else if (sight == 3)
+                map[i] = new Operator(x, y, 360.0, 270.0);
         }
 
         int[] counts = new int[N];
@@ -37,7 +71,24 @@ public class AL_27089 {
         int[] perm = new int[N];
 
         for (int i=0; i<N; i++) {
-            map[i].sight
+
+            for (int j = 0; j<N; j++) {
+                if (i==j) continue;
+
+                double deg = degrees(map[i], map[j]);
+            }
+
+            if (map[i].sight == 0) {
+                for (int j=0; j<N; j++) {
+
+                }
+            }
         }
     }
 }
+            /*
+            +x = 0
+            -x = 1
+            +y = 2
+            -y = 3
+             */
