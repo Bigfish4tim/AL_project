28e3b15ef372f57413abcf33484e835edc965959
Bigfish4tim <bigfish.4tim@gmail.com>
2023-08-11 17:58:43 +0900

    08-11

--- src/AL_27089.java
+++ src/AL_27089.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class AL_27089 {
 
-    static int N, K;
+    static int N;
+    static long K;
 
     public static class Operator {
         int x;
@@ -18,17 +19,18 @@ public class AL_27089 {
         }
     }
 
-    public static void angleCheck(int sight) {
-        /*
-        +x = 0
-        -x = 1
-        +y = 2
-        -y = 3
-         */
+    public static double calculateDistance(Operator a1, Operator a2) {
+        double deltaX = a2.x - a1.x;
+        double deltaY = a2.y - a1.y;
 
-        if (sight == 0) {
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
 
-        }
+    public static int taxiDistance(Operator a1, Operator a2) {
+        int deltaX = a2.x - a1.x;
+        int deltaY = a2.y - a1.y;
+
+        return Math.abs(deltaX) + Math.abs(deltaY);
     }
 
     public static double degrees(Operator a1, Operator a2) {
@@ -47,7 +49,7 @@ public class AL_27089 {
         Scanner scanner = new Scanner(System.in);
 
         N = scanner.nextInt();
-        K = scanner.nextInt();
+        K = scanner.nextLong();
 
         Operator[] map = new Operator[N];
 
@@ -57,38 +59,64 @@ public class AL_27089 {
             int sight = scanner.nextInt();
 
             if (sight == 0)
-                map[i] = new Operator(x, y, 90.0, 0.0);
+                map[i] = new Operator(x, y, 360.0, 270.0);
             else if (sight == 1)
                 map[i] = new Operator(x, y, 180.0, 90.0);
             else if (sight == 2)
-                map[i] = new Operator(x, y, 270.0, 180.0);
+                map[i] = new Operator(x, y, 90.0, 0.0);
             else if (sight == 3)
-                map[i] = new Operator(x, y, 360.0, 270.0);
+                map[i] = new Operator(x, y, 270.0, 180.0);
         }
 
         int[] counts = new int[N];
 
         int[] perm = new int[N];
 
-        for (int i=0; i<N; i++) {
+        int[] shootRange = new int[N];
 
-            for (int j = 0; j<N; j++) {
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
                 if (i==j) continue;
 
-                double deg = degrees(map[i], map[j]);
+                double deg = degrees(map[j], map[i]);
+
+                if (deg <= map[j].upSight && deg >= map[j].downSight)
+                    shootRange[i] += taxiDistance(map[j], map[i]);
             }
 
-            if (map[i].sight == 0) {
-                for (int j=0; j<N; j++) {
+            double dist = Double.MAX_VALUE;
+            for (int j = 0; j<N; j++) {
 
+                if (i == j) continue;
+
+                double deg = degrees(map[i], map[j]);
+
+                if (deg <= map[i].upSight && deg >= map[i].downSight) {
+                    double tempDist = calculateDistance(map[i], map[j]);
+                    if (dist > tempDist) {
+                        dist = tempDist;
+                        perm[i] = j;
+                    }
+                } else if (map[i].upSight == 360.0 && deg == 0.0) {
+                    double tempDist = calculateDistance(map[i], map[j]);
+                    if (dist > tempDist) {
+                        dist = tempDist;
+                        perm[i] = j;
+                    }
                 }
             }
         }
+
+        int point = 0;
+
+        for (int i=0; i<K; i++) {
+            counts[point] += shootRange[point];
+            point = perm[point];
+        }
+
+        for (int i=0; i<N; i++) {
+            System.out.println(counts[i]%998244353);
+        }
     }
 }
-            /*
-            +x = 0
-            -x = 1
-            +y = 2
-            -y = 3
-             */
+
