54261b78771837c29c28fe0aabd4c2a75960e568
Bigfish4tim <bigfish.4tim@gmail.com>
2023-08-30 16:36:57 +0900

    08-30

--- src/AL_1753.java
+++ src/AL_1753.java
@@ -1,81 +1,78 @@
 import java.util.*;
+import java.io.*;
 
 public class AL_1753 {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int INF = 100_000_000;
-    static int v,e,k;
-    static List<Node>[] list;
+    static ArrayList[] l;
     static int[] dist;
 
-    public class Node implements Comparable<Node>{
-        int end, weight;
+    public static class Node implements Comparable<Node>{
+        int v, w;
 
-        public Node(int end, int weight){
-            this.end = end;
-            this.weight = weight;
+        public Node(int v, int w){
+            this.v = v;
+            this.w = w;
         }
 
         @Override
-        public int compareTo(Node o) {
-            return weight - o.weight;
+        public int compareTo(Node n){
+            return this.w - n.w;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(br.readLine());
-        list = new ArrayList[v + 1];
-        dist = new int[v + 1];
 
-        Arrays.fill(dist, INF);
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine()) - 1;
 
-        for(int i = 1; i <= v; i++){
-            list[i] = new ArrayList<>();
+        l = new ArrayList[V];
+        dist = new int[V];
+
+        for(int i = 0; i<V; i++) {
+            l[i] = new ArrayList<Node>();
+            dist[i] = Integer.MAX_VALUE;
         }
-        // 리스트에 그래프 정보를 초기화
-        for(int i = 0 ; i < e; i++){
+
+        for(int i = 0; i<E; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            // start에서 end로 가는 weight 가중치
-            list[start].add(new Node(end, weight));
+
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+
+            l[u].add(new Node(v, w));
         }
+
+        dijkstra(K);
 
         StringBuilder sb = new StringBuilder();
-        // 다익스트라 알고리즘
-        dijkstra(k);
-        // 출력 부분
-        for(int i = 1; i <= v; i++){
-            if(dist[i] == INF) sb.append("INF\n");
-            else sb.append(dist[i] + "\n");
+        for(int i = 0; i<V; i++){
+            if(dist[i] == Integer.MAX_VALUE)
+                sb.append("INF").append("\n");
+            else
+                sb.append(dist[i]).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 
-    private static void dijkstra(int start){
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        boolean[] check = new boolean[v + 1];
-        queue.add(new Node(start, 0));
+    static void dijkstra(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         dist[start] = 0;
+        pq.add(new Node(start, 0));
 
-        while(!queue.isEmpty()){
-            Node curNode = queue.poll();
-            int cur = curNode.end;
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
 
-            if(check[cur] == true) continue;
-            check[cur] = true;
+            int len = l[now.v].size();
+            for(int i = 0; i<len; i++){
+                Node next = (Node)l[now.v].get(i);
 
-            for(Node node : list[cur]){
-                if(dist[node.end] > dist[cur] + node.weight){
-                    dist[node.end] = dist[cur] + node.weight;
-                    queue.add(new Node(node.end, dist[node.end]));
+                if(dist[next.v]>now.w + next.w){
+                    dist[next.v] = now.w + next.w;
+                    pq.add(new Node(next.v, dist[next.v]));
                 }
             }
         }
