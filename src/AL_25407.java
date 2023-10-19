34c0d4e52e53e24b90ada312c56f2b715286908a
Bigfish4tim <bigfish.4tim@gmail.com>
2023-10-19 16:44:33 +0900

    10-19

--- src/AL_25407.java
+++ src/AL_25407.java
@@ -6,10 +6,36 @@ public class AL_25407 {
 
     static int k;
 
+    public static class Node {
+        int node;
+        int distance;
+
+        Node(int node, int distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+    }
+
+    static Map<Integer, List<Node>> graph = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         N = scanner.nextInt();
+
+        for (int i=1; i<=N; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int i=2; i<=N; i++) {
+            int from = scanner.nextInt();
+            int length = scanner.nextInt();
+
+            graph.get(from).add(new Node(i, length));
+            graph.get(i).add(new Node(from, length));
+        }
+
+        System.out.println(graph.get(1));
 
     }
 }
